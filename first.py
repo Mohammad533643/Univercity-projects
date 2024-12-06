c6de92f55914f93cfee9023781d234ee3136068b
Mohammad533643 <m.mahdi.r.1400@gmail.com>
2024-12-06 12:20:50 +0330

    final file

--- first.py
+++ first.py
@@ -1,7 +1,26 @@
-t = int(input())
-
-for i in range(1):
-    x = max(map(int, input().split()))
-    # y = max(x)
+def factorail(x):
+    p = 1
+    for i in range(1,x+1):
+        p *= i
+    return p
     
-print(x)
+def combination(n,k):
+    c_answre = factorial(n)/((n-k)*(factoral(k))
+    return c_answre
+
+def final_answre(a,x,n,k):
+    y = (x**k)*(a**(n-k))
+    f_answre = combination(n,k) * y
+    return f_answre
+
+def run():
+    a,x,n = map(int,input().split())
+    k = 0
+    d = 0
+    while k<= n:
+        k += 1
+        d += final_answre(a,x,n,k)
+    print(d)
+    
+run()
+     
